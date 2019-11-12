3f17c0b5e1f2da1a0f242415e04b9637570adee6
nate-tomlin <48532338+nate-tomlin@users.noreply.github.com>
2019-11-12 15:08:38 -0500

    Update to clean_data.py
    This detects whether a image from a url has a face and prints true if it does and false if not

--- clean_data.py
+++ clean_data.py
@@ -1,3 +1,5 @@
+from skimage import io
+import cv2
 import csv
 import pandas as pd
 
@@ -5,4 +7,34 @@ import pandas as pd
 dataTrain = pd.read_csv("Data\\faceexp-comparison-data-train-public.csv", error_bad_lines=False)
 dataTest = pd.read_csv("Data\\faceexp-comparison-data-test-public.csv", error_bad_lines=False)
 
-print(dataTrain.iloc[[0],[0,5,10]])
+
+def CheckForFace(url):
+    image = io.imread(url)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #cv2.imshow('URL Image', image)
+    #cv2.waitKey()
+
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+    )
+
+    if ((len(faces)) == 0):
+        print("FASLE")
+        #return False
+    else:
+        print("TRUE")
+        #return True
+
+
+#print(dataTrain.iloc[:,[0,5,10]])
+
+url = "http://farm6.staticflickr.com/5033/14414157667_98383c7f1c_b.jpg"
+
+CheckForFace(url)
