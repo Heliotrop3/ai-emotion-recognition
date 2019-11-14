afaa095211aa3001ce2e7e9585cb636d17875c17
T <43817374+tyler-huffman@users.noreply.github.com>
2019-11-14 15:33:37 -0500

    Need to test clean_data to determine if issue with passing the entire dataset is resolved. Implemented a save function for saving the cleaned dataset.

--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,6 @@
-from skimage import io
-import cv2
-import csv
+import cv2, csv, urllib, getpass, os
 import pandas as pd
-import urllib
-import getpass
+from skimage import io
 
 '''
 Checks whether or not a face is present in the image
@@ -39,37 +36,36 @@ def CheckForFace(url):
 Given the FEC dataset, check whether the URL contains a face and if not remove the row from the csv
 '''
 def clean_data(data):
-    AFK
     '''
     Some image links appear in more then one row. In order to optimize the time taken in determining
     whether to delete a row we build a cache to store the result of CheckForFace where the url is the
     key and a boolean is the value.
     '''
+    image_links = data.iloc[:,[0,5,10]]            #Grab the columns where the urls for the image are stored
     link_dict = {}                                 #Create the cache
-    for index, row in data.iterrows():             #Iterate over all the rows in the csv 
+    for index, row in image_links.iterrows():      #Iterate over all the rows in the csv 
         for url in row:                            #Iterate over the links in each row
             if url not in link_dict.keys():        #If the url is not in the cache
                 link_dict[url] = CheckForFace(url) #Add it to the cache
-        ###print(row[0], row[1], row[2])           ###DEBUGGING: Check
+        ###print(row[0], row[1], row[2])           ###DEBUGGING: Print each link
         if (link_dict[row[0]] and link_dict[row[1]] and link_dict[row[2]]) != True:  #If any of the urls do not contain a face
-            data = data.drop([data.index[index]])  #Delete the row
+            data = data.drop([data.index[index]])  #Delete the row in the CSV
             print("Row {} Deleted".format(index))
 
     return data #Return the cleaned csv data
 
-#export_csv = df.to_csv (r'C:\Users\{}\Desktop\[Cleaned]text_emotions.csv'.format(getpass.getuser()), index = None, header=True)
+def save_csv(filename,data):
+    print("Saving {}.csv to {}".format(filename,path))
+    path = r"C:\Users\{}\Desktop\[Cleaned]{}.csv".format(getpass.getuser(),filename)
+    data.to_csv (path, index = None, header=True)
+    print("File Saved")
 
 
 def MCP():
     #Reads csv data files
     dataTrain = pd.read_csv("Data\\faceexp-comparison-data-train-public.csv", error_bad_lines=False)
     dataTest = pd.read_csv("Data\\faceexp-comparison-data-test-public.csv", error_bad_lines=False)
-    ###Grab the urls to the images
-    ###df = dataTrain.iloc[:,[0,5,10]]
-    '''
-    The problem with passing clean_data "dataTrain.iloc[:,[0,5,10]]" is it won't modify dataTrain accordingly.
-    We need to pass it the full dataset and then reference columns A,F, and K.
-    '''
     #dataTrain = clean_data(training_data)
-
+    #dataTest = clean_data(dataTest)
+    save_csv("Test",dataTrain)
 MCP()
