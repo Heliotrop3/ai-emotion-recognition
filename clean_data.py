259a260f74b6f2f13a38912a5e4797dc67fe5100
nate-tomlin <48532338+nate-tomlin@users.noreply.github.com>
2019-11-15 20:02:14 -0500

    Worked and can clean data for a given number of rows
    Fixes data.iloc[:,[0,5,10]]

--- clean_data.py
+++ clean_data.py
@@ -46,9 +46,11 @@ def clean_data(data):
     whether to delete a row we build a cache to store the result of CheckForFace where the url is the
     key and a boolean is the value.
     '''
-    image_links = data.iloc[:,[0,5,10]]            #Grab the columns where the urls for the image are stored
+    i = 0
+    numDeleted = 0
+    data = data.iloc[:,[0,5,10]]            #Grab the columns where the urls for the image are stored
     link_dict = {}                                 #Create the cache
-    for index, row in image_links.iterrows():      #Iterate over all the rows in the csv 
+    for index, row in data.iterrows():      #Iterate over all the rows in the csv 
         for url in row:                            #Iterate over the links in each row
             if url not in link_dict.keys():        #If the url is not in the cache
                 link_dict[url] = CheckForFace(url) #Add it to the cache
@@ -56,13 +58,22 @@ def clean_data(data):
         if (link_dict[row[0]] and link_dict[row[1]] and link_dict[row[2]]) != True:  #If any of the urls do not contain a face
             data = data.drop([data.index[index]])  #Delete the row in the CSV
             print("Row {} Deleted".format(index))
-
+            numDeleted += 1
+        i += 1
+        if i == 10:
+            print("Last Row: {}".format(index+1))
+            print("Number of Rows Deleted: {}".format(numDeleted))
+            ###for j in range(26, len(data.index)):
+             ###   data.drop(data.index[j])
+             ###   print("Row {} Deleted".format(j))
+            break
+        
     return data #Return the cleaned csv data
 
 def save_csv(filename,data):
+    path = r"C:\Users\{}\Desktop\[Cleaned]{}.csv".format(getpass.getuser(),filename)
     print("Saving {}.csv to {}".format(filename,path))
-    path = r"C:\\Users\\{}\\Desktop\\[Cleaned]{}.csv".format(getpass.getuser(),filename)
-    data.to_csv (path, index = None, header=True)
+    data.to_csv(path, index = None, header=True)
     print("File Saved")
 
 
@@ -72,5 +83,5 @@ def MCP():
     dataTest = pd.read_csv("Data\\faceexp-comparison-data-test-public.csv", error_bad_lines=False)
     dataTrain = clean_data(dataTrain)
     #dataTest = clean_data(dataTest)
-    save_csv("Test",dataTrain)
+    save_csv("TrainData",dataTrain)
 MCP()
