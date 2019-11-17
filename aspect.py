2b27582f601ea065d5d6b7f9d7b41b0ba3dc69ef
T <43817374+tyler-huffman@users.noreply.github.com>
2019-11-17 15:00:31 -0500

    Started overhauling following the tensorflow tutorial for image classification

--- aspect.py
+++ aspect.py
@@ -1,20 +1,27 @@
-from fastai import *
-from fastai.text import *
-from fastai.core import *
-import tensorflow
-import torch
-import cv2
-import csv
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
 import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-#path = 'C:\\Users\\%USERNAME%\\Documents\\GitHub\\ai-emotion-recognition\\Data'
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Define the paths to the respective data sets
+train_file = r"Data\faceexp-comparison-data-train-public.csv"
+test_file  = r"Data\\faceexp-comparison-data-test-public.csv" 
 
 #Reads csv data files
-dataTrain = pd.read_csv("Data\\faceexp-comparison-data-train-public.csv", error_bad_lines=False)
-dataTest = pd.read_csv("Data\\faceexp-comparison-data-test-public.csv", error_bad_lines=False)
+train_data = pd.read_csv(train_file, error_bad_lines=False)
+test_data  = pd.read_csv(test_file, error_bad_lines=False)
 
-fiveDataTrain = dataTrain.head()
-print(fiveDataTrain)
+#Print the size of the data sets by printing the number of rows
+#and multiplying the result by 3 as there are 3 images per row
+print("Number of Training Images  : {}".format(train_data.shape[0]*3))
+print("Number of Validation Images: {}".format(test_data.shape[0]*3))
 
-fiveDataTest = dataTest.head()
-print(fiveDataTest)
+batch_size = 128
+epochs = 15
+#Need to determine image height and width
