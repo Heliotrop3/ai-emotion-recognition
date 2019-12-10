c660f8367a43fb2a7b520ba1dbc2de1e11a2207e
nate-tomlin <48532338+nate-tomlin@users.noreply.github.com>
2019-12-10 12:07:21 -0500

    Got Confusion Matrix to work

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -9,10 +9,12 @@ from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from fastai.vision import *
+from mlxtend.plotting import plot_confusion_matrix
+import matplotlib.pyplot as plt
 
 
 BASEPATH = r'C:\Users\{}\Documents\GitHub\ai-emotion-recognition'.format(getpass.getuser())
-data = pd.read_csv(r'{}\Data\fer2013.csv'.format(BASEPATH))
+data = pd.read_csv(r'{}\DataSet\fer2013.csv'.format(BASEPATH))
 model = load_model(r"Models\model_64_40Epochs.h5")
 
 pixels = data['pixels'].tolist() # 1
@@ -44,18 +46,26 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.
 #print("Loss: " + str(scores[0]))
 #print("Accuracy: " + str(scores[1]))
 
-y_pred = model.predict_classes(np.array(X_test), batch_size=batch_size, verbose=0)
-#y_pred = max(y_pred)
-#y_pred = (y_pred > .5 )
-#y_pred = np.argmax(y_pred, axis=1)
-confusion = confusion_matrix(np.array(y_test), np.array(y_pred))
+
+y_pred = model.predict_classes(X_test)
+#print(y_pred)
+#print(y_test)
+new_y_test = []
+value_list = []
+for row_list in y_test:
+    for  i in range(len(row_list)):
+        value_list = [(i, row_list[i])]
+        if row_list[i] == 1:
+            #print(i)
+            new_y_test.append(i)
+    #print([list((i, row_list[i])) for i in range(len(row_list))]) 
+    
+labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+#print(new_y_test)
+confusion = confusion_matrix(new_y_test, y_pred)
+fig, ax = plot_confusion_matrix(conf_mat=confusion,
+                                colorbar=True,
+                                class_names=labels)
+plt.show()
 print(confusion)
 
-
-#print(X_test)
-#Y_pred = model.predict_generator((np.array(X_test), np.array(y_test)), 7174 // batch_size+1)
-#y_pred = np.argmax(Y_pred, axis=1)
-#print('Confusion Matrix')
-#print(confusion_matrix((np.array(X_test), np.array(y_test)).classes, y_pred))
-
-#print(confusion_matrix(y_test, y_val))
